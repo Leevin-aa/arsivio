42f8009cbf888d6b270928a667ced2be28aaf4f2
radhiiiy <radhiyyan232@gmail.com>
2025-12-29 17:17:35 +0800

    responsive mobile

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const hamburger = document.getElementById("hamburger");
   const navMenu = document.getElementById("navMenu");
 
@@ -8,12 +7,4 @@ document.addEventListener("DOMContentLoaded", () => {
   hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
   });
-
-  // Reset menu saat resize ke desktop
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 768) {
-      navMenu.classList.remove("active");
-    }
-  });
-
 });
