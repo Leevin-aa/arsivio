2b4db6412187cfbce00d502b26301ba43af1ac40
radhiiiy <radhiyyan232@gmail.com>
2025-12-29 14:51:48 +0800

    finished street view

--- script.js
+++ script.js
@@ -20,14 +20,13 @@ const viewer = pannellum.viewer('panorama', {
             maxHfov: 110,
             pitch: 0,
             yaw: 0,
-            compass: true,
             hotSpots: [
                 {
                     pitch: 0,
                     yaw: 8,
                     type: "scene",
                     sceneId: "ruang2",
-                    text: ""
+                    text: "ruang2"
                 }
             ]
         },
@@ -41,28 +40,27 @@ const viewer = pannellum.viewer('panorama', {
             maxHfov: 110,
             pitch: 0,
             yaw: 0,
-            compass: true,
             hotSpots: [
                 {
                     pitch: -2,
                     yaw: 8,
                     type: "scene",
                     sceneId: "ruang1",
-                    text: ""
+                    text: "ruang1"
                 },
                 {
                     pitch: -2,
                     yaw: -100,
                     type: "scene",
                     sceneId: "ruang3",
-                    text: ""
+                    text: "ruang3"
                 },
                 {
                     pitch: -2,
                     yaw: -50,
                     type: "scene",
                     sceneId: "ruang4",
-                    text: ""
+                    text: "ruang4"
                 }
             ],
         },
@@ -75,42 +73,41 @@ const viewer = pannellum.viewer('panorama', {
             maxHfov: 110,
             pitch: 0,
             yaw: 0,
-            compass: true,
             hotSpots: [
                 {
                     pitch: -2,
                     yaw: 10,
                     type: "scene",
-                    sceneId: "",
-                    text: ""
+                    sceneId: "ruang8",
+                    text: "ruang8"
                 },
                 {
                     pitch: -2,
                     yaw: -100,
                     type: "scene",
                     sceneId: "ruang2",
-                    text: ""
+                    text: "ruang2"
                 },
                 {
                     pitch: -2,
                     yaw: 120,
                     type: "scene",
-                    sceneId: "",
-                    text: ""
+                    sceneId: "ruang7",
+                    text: "ruang7"
                 },
                 {
                     pitch: -2,
                     yaw: 60,
                     type: "scene",
-                    sceneId: "",
-                    text: ""
+                    sceneId: "ruang9",
+                    text: "ruang9"
                 },
                 {
                     pitch: -2,
                     yaw: 210,
                     type: "scene",
                     sceneId: "ruang4",
-                    text: ""
+                    text: "ruang4"
                 }
             ],
         },
@@ -123,28 +120,27 @@ const viewer = pannellum.viewer('panorama', {
             maxHfov: 110,
             pitch: 0,
             yaw: 0,
-            compass: true,
             hotSpots: [
                 {
                     pitch: 0,
                     yaw: -90,
                     type: "scene",
                     sceneId: "ruang5",
-                    text: ""
+                    text: "ruang5"
                 },
                 {
                     pitch: 0,
                     yaw: 180,
                     type: "scene",
                     sceneId: "ruang2",
-                    text: ""
+                    text: "ruang2"
                 },
                 {
                     pitch: 0,
                     yaw: 190,
                     type: "scene",
                     sceneId: "ruang3",
-                    text: ""
+                    text: "ruang3"
                 },
             ]
         },
@@ -157,21 +153,20 @@ const viewer = pannellum.viewer('panorama', {
             maxHfov: 110,
             pitch: 0,
             yaw: 0,
-            compass: true,
             hotSpots: [
                 {
                     pitch: 0,
                     yaw: -80,
                     type: "scene",
                     sceneId: "ruang4",
-                    text: "",
+                    text: "ruang4",
                 },
                 {
                     pitch: 0,
                     yaw: 100,
                     type: "scene",
                     sceneId: "ruang6",
-                    text: ""
+                    text: "ruang6",
                 },
             ]
         },
@@ -184,22 +179,21 @@ const viewer = pannellum.viewer('panorama', {
             maxHfov: 110,
             pitch: 0,
             yaw: 0,
-            compass: true,
             hotSpots: [
                 {
                     pitch: 0,
                     yaw: 130,
                     type: "scene",
                     sceneId: "ruang5",
-                    text: ""
-                    
+                    text: "ruang5"
+
                 },
                 {
                     pitch: 0,
                     yaw: -145,
                     type: "scene",
                     sceneId: "ruang7",
-                    text: ""
+                    text: "ruang7"
                 }
             ]
         },
@@ -212,16 +206,200 @@ const viewer = pannellum.viewer('panorama', {
             maxHfov: 110,
             pitch: 0,
             yaw: 0,
-            compass: true,
             hotSpots: [
                 {
                     pitch: 0,
                     yaw: 130,
                     type: "scene",
-                    sceneId: "",
-                    text: ""
+                    sceneId: "ruang3",
+                    text: "ruang3"
+                },
+                {
+                    pitch: 0,
+                    yaw: 35,
+                    type: "scene",
+                    sceneId: "ruang6",
+                    text: "ruang6"
                 }
             ]
+        },
+        ruang8: {
+            title: "Arsivio.st",
+            type: "equirectangular",
+            panorama: "fotostreetview/utama8.png",
+            hfov: 200,
+            minHfov: 50,
+            maxHfov: 110,
+            pitch: 0,
+            yaw: 0,
+            hotSpots: [
+                {
+                    pitch: 0,
+                    yaw: 105,
+                    type: "scene",
+                    sceneId: "ruang3",
+                    text: "ruang3"
+                },
+                {
+                    pitch: 0,
+                    yaw: -95,
+                    type: "scene",
+                    sceneId: "ruang11",
+                    text: "ruang11"
+                },
+                {
+                    pitch: 0,
+                    yaw: -160,
+                    type: "scene",
+                    sceneId: "ruang10",
+                    text: "ruang10"
+                }
+            ]
+        },
+        ruang9: {
+            title: "Arsivio.st",
+            type: "equirectangular",
+            panorama: "fotostreetview/utama9.png",
+            hfov: 200,
+            minHfov: 50,
+            maxHfov: 110,
+            pitch: 0,
+            yaw: 0,
+            hotSpots: [
+                {
+                    pitch: 0,
+                    yaw: -20,
+                    type: "scene",
+                    sceneId: "ruang3",
+                    text: "ruang3"
+                },
+                
+            ]
+        },
+        ruang10: {
+            title: "Arsivio.st",
+            type: "equirectangular",
+            panorama: "fotostreetview/utama10.png",
+            hfov: 200,
+            minHfov: 50,
+            maxHfov: 110,
+            pitch: 0,
+            yaw: 0,
+            hotSpots: [
+                {
+                    pitch: 0,
+                    yaw: 110,
+                    type: "scene",
+                    sceneId: "ruang8",
+                    text: "ruang8"
+                },
+                {
+                    pitch: 0,
+                    yaw: 35,
+                    type: "scene",
+                    sceneId: "ruang11",
+                    text: "ruang11"
+                }
+            ]
+        },
+        ruang11: {
+            title: "Arsivio.st",
+            type: "equirectangular",
+            panorama: "fotostreetview/utama11.png",
+            hfov: 200,
+            minHfov: 50,
+            maxHfov: 110,
+            pitch: 0,
+            yaw: 0,
+            hotSpots: [
+                {
+                    pitch: 0,
+                    yaw: 135,
+                    type: "scene",
+                    sceneId: "ruang8",
+                    text: "ruang8"
+                },
+                {
+                    pitch: 0,
+                    yaw: -125,
+                    type: "scene",
+                    sceneId: "ruang12",
+                    text: "ruang12"
+                },
+            ]
+        },
+        ruang12: {
+            title: "Arsivio.st",
+            type: "equirectangular",
+            panorama: "fotostreetview/utama12.png",
+            hfov: 200,
+            minHfov: 50,
+            maxHfov: 110,
+            pitch: 0,
+            yaw: 0,
+            hotSpots: [
+                {
+                    pitch: 0,
+                    yaw: -175,
+                    type: "scene",
+                    sceneId: "ruang13",
+                    text: "ruang13"
+                },
+                {
+                    pitch: 0,
+                    yaw: 43,
+                    type: "scene",
+                    sceneId: "ruang11",
+                    text: "ruang11"
+                },
+            ]
+        },
+        ruang13: {
+            title: "Arsivio.st",
+            type: "equirectangular",
+            panorama: "fotostreetview/utama13.png",
+            hfov: 200,
+            minHfov: 50,
+            maxHfov: 110,
+            pitch: 0,
+            yaw: 0,
+            hotSpots: [
+                {
+                    pitch: 0,
+                    yaw: 110,
+                    type: "scene",
+                    sceneId: "ruang12",
+                    text: "ruang12"
+                },
+                {
+                    pitch: 0,
+                    yaw: -70,
+                    type: "scene",
+                    sceneId: "ruang14",
+                    text: "ruang14"
+                }
+            ]
+        },
+        ruang14: {
+            title: "Arsivio.st",
+            type: "equirectangular",
+            panorama: "fotostreetview/utama14.png",
+            hfov: 200,
+            minHfov: 50,
+            maxHfov: 110,
+            pitch: 0,
+            yaw: 0,
+            hotSpots: [
+                {
+                    pitch: 0,
+                    yaw: 115,
+                    type: "scene",
+                    sceneId: "ruang13",
+                    text: "ruang13"
+                }
+                
+            ]
         }
+        
     }
 });
