487815b3fbae9109d20b8e03f714ee9768c7c9e0
radhiiiy <radhiyyan232@gmail.com>
2026-01-29 09:52:03 +0700

    Rename Change

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 8,
                     type: "scene",
                     sceneId: "ruang2",
-                    text: "ruang2"
+                    text: "Klasik"
                 }
             ]
         },
@@ -45,21 +45,21 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 8,
                     type: "scene",
                     sceneId: "ruang1",
-                    text: "ruang1"
+                    text: "Klasik"
                 },
                 {
                     pitch: -2,
                     yaw: -100,
                     type: "scene",
                     sceneId: "ruang3",
-                    text: "ruang3"
+                    text: "Hindu - Budha"
                 },
                 {
                     pitch: -2,
                     yaw: -50,
                     type: "scene",
                     sceneId: "ruang4",
-                    text: "ruang4"
+                    text: "Prasejarah"
                 }
             ],
         },
@@ -78,35 +78,35 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 10,
                     type: "scene",
                     sceneId: "ruang8",
-                    text: "ruang8"
+                    text: "Kolonial"
                 },
                 {
                     pitch: -2,
                     yaw: -100,
                     type: "scene",
                     sceneId: "ruang2",
-                    text: "ruang2"
+                    text: "Klasik"
                 },
                 {
                     pitch: -2,
                     yaw: 120,
                     type: "scene",
                     sceneId: "ruang7",
-                    text: "ruang7"
+                    text: "Hindu - Budha"
                 },
                 {
                     pitch: -2,
                     yaw: 60,
                     type: "scene",
                     sceneId: "ruang9",
-                    text: "ruang9"
+                    text: "islam"
                 },
                 {
                     pitch: -2,
                     yaw: 210,
                     type: "scene",
                     sceneId: "ruang4",
-                    text: "ruang4"
+                    text: "Kolonial"
                 }
             ],
         },
@@ -125,21 +125,21 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: -90,
                     type: "scene",
                     sceneId: "ruang5",
-                    text: "ruang5"
+                    text: "Hindu - Budha"
                 },
                 {
                     pitch: 0,
                     yaw: 180,
                     type: "scene",
                     sceneId: "ruang2",
-                    text: "ruang2"
+                    text: "Klasik"
                 },
                 {
                     pitch: 0,
                     yaw: 190,
                     type: "scene",
                     sceneId: "ruang3",
-                    text: "ruang3"
+                    text: "Hindu - Budha"
                 },
             ]
         },
@@ -158,14 +158,14 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: -80,
                     type: "scene",
                     sceneId: "ruang4",
-                    text: "ruang4",
+                    text: "Klasik",
                 },
                 {
                     pitch: 0,
                     yaw: 100,
                     type: "scene",
                     sceneId: "ruang6",
-                    text: "ruang6",
+                    text: "Hindu - Budha",
                 },
             ]
         },
@@ -184,7 +184,7 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 130,
                     type: "scene",
                     sceneId: "ruang5",
-                    text: "ruang5"
+                    text: "Hindu - Budha"
 
                 },
                 {
@@ -192,7 +192,7 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: -145,
                     type: "scene",
                     sceneId: "ruang7",
-                    text: "ruang7"
+                    text: "Hindu - Budha"
                 }
             ]
         },
@@ -211,14 +211,14 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 130,
                     type: "scene",
                     sceneId: "ruang3",
-                    text: "ruang3"
+                    text: "Klasik"
                 },
                 {
                     pitch: 0,
                     yaw: 35,
                     type: "scene",
                     sceneId: "ruang6",
-                    text: "ruang6"
+                    text: "Hindu - Budha"
                 }
             ]
         },
@@ -237,21 +237,21 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 105,
                     type: "scene",
                     sceneId: "ruang3",
-                    text: "ruang3"
+                    text: "Hindu - Budha"
                 },
                 {
                     pitch: 0,
                     yaw: -95,
                     type: "scene",
                     sceneId: "ruang11",
-                    text: "ruang11"
+                    text: "Banyuwangi Tempoe Doeloe"
                 },
                 {
                     pitch: 0,
                     yaw: -160,
                     type: "scene",
                     sceneId: "ruang10",
-                    text: "ruang10"
+                    text: "Kolonial"
                 }
             ]
         },
@@ -270,7 +270,7 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: -20,
                     type: "scene",
                     sceneId: "ruang3",
-                    text: "ruang3"
+                    text: "Hindu - Budha"
                 },
                 
             ]
@@ -290,14 +290,14 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 110,
                     type: "scene",
                     sceneId: "ruang8",
-                    text: "ruang8"
+                    text: "Klasik"
                 },
                 {
                     pitch: 0,
                     yaw: 35,
                     type: "scene",
                     sceneId: "ruang11",
-                    text: "ruang11"
+                    text: "Banyuwangi Tempoe Doeloe"
                 }
             ]
         },
@@ -316,14 +316,14 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 135,
                     type: "scene",
                     sceneId: "ruang8",
-                    text: "ruang8"
+                    text: "Klasik"
                 },
                 {
                     pitch: 0,
                     yaw: -125,
                     type: "scene",
                     sceneId: "ruang12",
-                    text: "ruang12"
+                    text: "Banyuwangi Tempoe Doeloe"
                 },
             ]
         },
@@ -342,14 +342,14 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: -175,
                     type: "scene",
                     sceneId: "ruang13",
-                    text: "ruang13"
+                    text: "Banyuwangi Tempoe Doeloe"
                 },
                 {
                     pitch: 0,
                     yaw: 43,
                     type: "scene",
                     sceneId: "ruang11",
-                    text: "ruang11"
+                    text: "Banyuwangi Tempoe Doeloe"
                 },
             ]
         },
@@ -368,14 +368,14 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 110,
                     type: "scene",
                     sceneId: "ruang12",
-                    text: "ruang12"
+                    text: "Banyuwangi Tempoe Doeloe"
                 },
                 {
                     pitch: 0,
                     yaw: -70,
                     type: "scene",
                     sceneId: "ruang14",
-                    text: "ruang14"
+                    text: "Banyuwangi Tempoe Doeloe"
                 }
             ]
         },
@@ -394,7 +394,7 @@ const viewer = pannellum.viewer('panorama', {
                     yaw: 115,
                     type: "scene",
                     sceneId: "ruang13",
-                    text: "ruang13"
+                    text: "Banyuwangi Tempoe Doeloe"
                 }
                 
             ]
